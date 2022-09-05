059a4a43b2d5921df38ddd58ad69864c800b08b3
moinSebi <s.vorbrugg@googlemail.com>
2022-09-05 17:01:52 +0200

    Helper test fix.

--- src/core/helper.rs
+++ src/core/helper.rs
@@ -17,11 +17,10 @@ pub fn hashset2string(input: &HashSet<u32>, sep:  &str) -> String{
 
 #[cfg(test)]
 mod helpertest {
-    use crate::core::helper::{bool2string_dir, vec2string};
+    use crate::core::helper::{vec2string};
 
     #[test]
     fn helpers() {
-        assert_eq!(bool2string_dir(true), "+");
         let k: Vec<u32> = vec![1,2,3,4];
         assert_eq!(vec2string(&k, "."), "1.2.3.4".to_string());
     }
